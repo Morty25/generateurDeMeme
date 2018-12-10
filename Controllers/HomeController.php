ab2cb4d1fea6b23029bf7ee72635ce95a63d296b
rainbow59 <laura.vangrevenynghe@laposte.net>
2018-12-10 16:50:39 +0100

    Laura-> PUSH dossier Controllers qui contient HomeController.php et ImagesController.php

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,26 +1,11 @@
 <?php
-include('Models/Home.php');
+include('Models/Images.php');
+
 $images = afficheImages();
 
-//inclure la vue
 include('Views/HomeView.php');
 
 
 
-//last time <?php
-
-// //le controlleur inclut le modèle
-// include('Models/Film.php');
 
 
-
-// //Récupérer les données (ici, tous les films car on est sur la home)
-// $movies = getAllMovies();
-
-// //traiter les données
-// foreach ($movies as $key => $movie) {
-//     $movie['realisateur'] = ucfirst($movie['realisateur']); //Mets une majuscule au nom du réalisateur
-// }
-
-// //inclure la vue
-// include('Views/HomeView.php');
