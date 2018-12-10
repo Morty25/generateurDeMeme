34bbe8663dc18268cbe431eab18bef0e11b36b0e
rainbow59 <laura.vangrevenynghe@laposte.net>
2018-12-10 16:54:18 +0100

    Laura-> PUSH dossier Models qui contient Images.php

--- Models/Images.php
+++ Models/Images.php
@@ -1,22 +1,27 @@
 <?php
-            try
-            {
-                // On se connecte à MySQL
-                $bdd = new PDO('mysql:host=localhost;dbname=generator', 'root', '',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-            }
-            catch(Exception $e)
-            {
-                // En cas d'erreur, on affiche un message et on arrête tout
-                    die('Erreur : '.$e->getMessage());
-            }
+/*  On se connecte à la BDD  */
+    try
+    {
+        $bdd = new PDO("mysql:host=localhost;dbname=generator;charset=UTF8", "root", "Lolorainbow!");
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
             
     
-        /*  Fonction pour afficher les images de la bibliothèque  */ 
-        function afficheImages() {
+/*  Fonction pour afficher les images de la bibliothèque  */ 
+    function afficheImages() {
         global $bdd;
-        // $images=nom de veriable , image=nom de table en sql
-        $images = $bdd->query('SELECT * FROM images');
-
+        $images = $bdd->query('SELECT url FROM images');
         return $images->fetchAll();
+    } 
 
-        }
+
+/* Fonction pour stocker les mm réalisés (insert titre + image) */
+    function stockMM($chemin) {
+        global $bdd;
+        $MM= $bdd->prepare("INSERT INTO mm ('titre', 'image') VALUES (?, ?)");
+        return $MM->execute(array($_POST['titre'], $_POST['image']));
+        // $id_MMrecuperer = $bdd->lastInsertId();
+    }
