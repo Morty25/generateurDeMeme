b25dd2d155279df257e1a2c448edaa5127579e54
anjum18 <anjumandleeb@gmail.com>
2018-12-13 16:59:39 +0100

    ok

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,7 +1,7 @@
 
 <!-- Pas de Balise HTML dans le controller -->
 <?php
-include('Models/Accuile.php');
+include('Models/Accueil.php');
 // calling the function gelAllMeme to use them for HomeView.php
 $images = getAllMeme();
 include('Views/HomeView.php');
